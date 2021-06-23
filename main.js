c7dab69d2205c57e3b11376d0e632f4e920a38c4
Krish734-arch <83488772+Krish734-arch@users.noreply.github.com>
2021-06-23 21:00:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -89,3 +89,67 @@ window.addEventListener("keydown" , my_keydown);
             console.log("t");
         }
     }
+
+    function up(){
+        if(car1_y>=0)
+        car1_y-=10;
+        console.log("When up arrow is pressed,x=" +car1_x+",y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+    }
+
+    function down(){
+        if(car1_y<=500)
+        car1_y+=10;
+        console.log("When down arrow is pressed,x=" +car1_x+",y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+    }
+
+    function left(){
+        if(car1_x>=0)
+        car1_x-=10;
+        console.log("When left arrow is pressed,x=" +car1_x+",y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+    }
+
+    function right(){
+        if(car1_x<=700)
+        car1_x+=10;
+        console.log("When right arrow pressed,x=" +car1_x+",y="+car1_y);
+        uploadBackground();
+        uploadcar1();
+    }
+
+    function up(){
+        if(car2_y>=0)
+        car2_y-=10;
+        console.log("When up arrow is pressed,x=" +car2_x+",y="+car2_y);
+        uploadBackground();
+        uploadcar2();
+    }
+
+    function down(){
+        if(car2_y<=500)
+        car2_y+=10;
+        console.log("When down arrow is pressed,x=" +car2_x+",y="+car2_y);
+        uploadBackground();
+        uploadcar2();
+    }
+
+    function left(){
+        if(car2_x>=0)
+        car2_x-=10;
+        console.log("When left arrow is pressed,x=" +car2_x+",y="+car2_y);
+        uploadBackground();
+        uploadcar2();
+    }
+
+    function right(){
+        if(car2_x<=700)
+        car2_x+=10;
+        console.log("When right arrow pressed,x=" +car2_x+",y="+car2_y);
+        uploadBackground();
+        uploadcar2();
+    }
